d161d607687197c10027954faaf56527d05f0dbf
Jacob Van Vuuren <jacob.vanvuuren@gmail.com>
2022-03-05 13:19:14 -0500

    Update activeadmin_config.rb

--- lib/activeadmin_jobs/activeadmin_config.rb
+++ lib/activeadmin_jobs/activeadmin_config.rb
@@ -15,13 +15,18 @@ ActiveAdmin.application.load_paths += [File.join(ActiveadminJobs::Engine.root, "
 
 class ActiveAdmin::Views::Pages::Base
   def build(*args)
-    super(args)
+    # copy of build - START
+    set_attribute :lang, I18n.locale
+    build_active_admin_head
+    build_page
+    # copy of build - END
+
+    body = get_elements_by_tag_name("body").first
+
     current_user_method = ActiveAdmin.application.current_user_method
 
     if current_user_method
       admins_job_identifier = send(current_user_method).job_identifier
-      
-      body = get_elements_by_tag_name("body").first
       body.set_attribute "data-identifier", admins_job_identifier
       body.set_attribute "data-root-url", "/"
       body.set_attribute "data-jobs-url", AdminHelpers.jobs_url
